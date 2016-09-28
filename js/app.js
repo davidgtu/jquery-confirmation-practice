d12c722286ca83162f2f62151bad2333906f3f83
spacebear5000 <david.giahuy.tu@gmail.com>
2016-09-28 03:22:21 -0400

    insert pseudocode

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//hints are shown when form is valid
+//hide and show them at appropiate times
+
+// hide hints
+
+//if password is valid
+  //hide hint
+  //else show hint
+
+//when event happens on confirmation
+  //find out if password === confirmation
+    //hide hint if match
+    //else show hint
